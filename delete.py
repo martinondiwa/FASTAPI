810f8460128c0dbac48c225cf32ec865a42a04b9
martinondiwa <surekondiwa@gmail.com>
2025-04-25 19:30:42 +0300

    asd

--- delete.py
+++ delete.py
@@ -14,7 +14,15 @@ def find_index_post(id: int):
         if p ['id'] == id:
             return i
 
-@app.delete("/posts/{id}")
+#@app.get("/posts/{id}")
+#def get_post(id: int):
+
+ #   post = find_post(id)
+  #  if not post:
+   #     raise HTTPException(status_code=status.HTTP_204_NO_CONTENT)
+                    
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int):
     #deleting post
     # find the index in the array that has required ID
