c0f2bd491c50978cd85009968c40250fc1601f65
martinondiwa <surekondiwa@gmail.com>
2025-04-25 18:43:45 +0300

    asd

---
+++ delete.py
@@ -0,0 +1,21 @@
+
+from fastapi import FastAPI,
+
+app = FastAPI()
+
+#Deleting posts
+def find_index_post(id):
+    for i, p in enumarate (my_posts):
+        if p ['id'] == id:
+            return i
+
+@app.delete("/posts/{id}")
+def delete_post():
+    #deleting post
+    # find the index in the array that has required ID
+    #my_posts.pop(index)
+    index = find_index_post(id)
+
+    my_posts.pop(index)
+
+    return {"message": "was successfully deleted"}
