8e02e25e63c54e3974652c05b9b4540e66ad608b
martinondiwa <surekondiwa@gmail.com>
2025-04-25 07:26:28 +0300

    ass

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/") '''Decorator'''
 def root():
+
         return {"message": "Hello World"}
+
+
+@app.get("/posts/vote") '''specifies a different path'''
+def root():
+    
+    return()
