b0e09528cbeaeda4984bc56607eeaf98de8f8f3f
martinondiwa <surekondiwa@gmail.com>
2025-04-25 19:17:30 +0300

    asdf

--- delete.py
+++ delete.py
@@ -1,10 +1,15 @@
 from main import app
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException
 
 app = FastAPI()
 
+my_posts = [
+        {"id": 1, "title": "Post One", "content": "This is post one"},
+        {"id": 2, "title": "Post Two", "content": "This is post two"},
+        ]
+
 #Deleting posts
-def find_index_post(id):
+def find_index_post(id: int):
     for i, p in enumerate(my_posts):
         if p ['id'] == id:
             return i
