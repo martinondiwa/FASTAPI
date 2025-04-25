a9321c7cc8dbfd85785df5fc6014e67bcdb4f182
martinondiwa <surekondiwa@gmail.com>
2025-04-25 15:56:22 +0300

    asd

--- main.py
+++ main.py
@@ -125,7 +125,7 @@ def create_posts(post: post):
 @app.get("/posts/{id}")
 def get_post(id: int, response:Response):
     
-    post = find_post(int(id))
+    post = find_post(id)
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail = f"post with id: {id} was not found")
