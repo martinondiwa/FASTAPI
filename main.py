77ca3972502496a3e4b53674302cb82e6fef0b66
martinondiwa <surekondiwa@gmail.com>
2025-04-25 09:55:27 +0300

    asd

--- main.py
+++ main.py
@@ -10,3 +10,7 @@ async def root():
 @app.get("/posts")
 def get_posts():
     return{"data": "This is your posts"}
+
+@app.post("createposts")
+def create_posts():
+    return {"message": "successfully created posts"}
