24f48c8ab3d5f9101b4ad5f6841dec1fbcb17177
martinondiwa <surekondiwa@gmail.com>
2025-04-25 16:11:48 +0300

    asd

--- main.py
+++ main.py
@@ -129,4 +129,4 @@ def get_post(id: int, response: Response):
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail = f"post with id: {id} was not found")
-        return {"post_detail": post}
+    return {"post_detail": post}
