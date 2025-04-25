c6f0231421e3eefd427fea7ec95618d5f802afda
martinondiwa <surekondiwa@gmail.com>
2025-04-25 10:36:40 +0300

    asd

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+class post(BaseModel):
+    title: str
+    content: str
 
 @app.get("/")
 async def root():
@@ -13,7 +16,13 @@ async def root():
 def get_posts():
     return{"data": "This is your posts"}
 
+#@app.post("/createposts")
+#def create_posts(payLoad: dict = Body(...)):
+ #   print(payLoad)
+  #  return {"new_post": f"title {payLoad['title']} content: {payLoad['content']}"}
+
+# title str, content str 
 @app.post("/createposts")
-def create_posts(payLoad: dict = Body(...)):
-    print(payLoad)
-    return {"new_post": f"title {payLoad['title']} content: {payLoad['content']}"}
+def create_posts(new_post: post):
+        print(new_post)
+            return {"data": "new post"}
